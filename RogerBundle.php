b579b0aba6bda6d554594416dfcadce7793558a6
roger <rpedrol@holmusic.es>
2024-11-25 00:29:26 +0100

    canvi de nom, roger ocupat

--- RogerBundle.php
+++ RogerBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Roger\Bundle\RogerBundle;
+namespace Xtura\Bundle\RogerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -13,6 +13,6 @@ class RogerBundle extends Bundle
 
   public function getPath(): string
   {
-      return \dirname(__DIR__, 3).'/Roger/Bundle/RogerBundle';
+      return \dirname(__DIR__, 3).'/Xtura/Bundle/RogerBundle';
   }
 }
